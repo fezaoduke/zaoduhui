189a2dbb682a034388091347cde79b6fdb990bd7
Zelda Zhang <ze.zhang@hotmail.com>
2017-02-09 14:42:01 +0800

    flux实现

---
+++ React全栈/杭州-张大侠@第三期/demo/demo-flux/actions/TodoAction.js
@@ -0,0 +1,24 @@
+//action可以看做是应用程序的各种交互动作，而每一个动作产生后（例如新建或者删除）都会交给Dispatcher这个调度中心来处理
+
+import AppDispatcher from '../dispatcher/AppDispatcher';
+
+const TodoAction = {
+  create(todo) {
+    AppDispatcher.dispatch(//交给Dispatcher
+    	
+	    //Action本质是一个普通的JavaScript Object
+	    {	
+	      actionType: 'CREATE_TODO',	//actionType字段表明这个action的用途
+	      todo: todo	//另一个字段表明它所传递的信息
+	    }
+    );
+  },
+  delete(id) {
+    AppDispatcher.dispatch({	//交给Dispatcher
+      actionType: 'DELETE_TODO',
+      id: id
+    });
+  }
+};
+
+export default TodoAction;
---
+++ React全栈/杭州-张大侠@第三期/demo/demo-flux/dispatcher/AppDispatcher.js
@@ -0,0 +1,5 @@
+//Dispatcher可以看作是一个调度中心
+//Dispatcher有Facebook的官方实现，称之为Flux Dispatcher
+import { Dispatcher } from 'flux';
+
+export default new Dispatcher();	
